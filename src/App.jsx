58bfa60775b02d3126300d1a94c2ad89d137917d
jordankamto <jordankamto32@gmail.com>
2025-02-23 23:37:14 +0100

    initial project set-up with vite-react template and mui library

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button variant="contained">Hello world</Button>
+    </>
+  );
+};
+
+export default App;
